dec39c19777c139fdf96df8c17c613ac276fdd92
Hậu Trần <hautrantrung.02@gmail.com>
2025-05-27 23:12:00 +0700

    form complete

--- src/app/shared/directives/typography.directive.ts
+++ src/app/shared/directives/typography.directive.ts
@@ -4,6 +4,7 @@ import {
   Input,
   Renderer2,
   OnChanges,
+  SimpleChanges,
 } from '@angular/core';
 
 @Directive({
@@ -11,24 +12,25 @@ import {
   standalone: false,
 })
 export class TypographyDirective implements OnChanges {
-  @Input('appTypography') variant:
-    | 'h1'
-    | 'h2'
-    | 'h3'
-    | 'h4'
-    | 'h5'
-    | 'h6'
-    | 'p'
-    | 'small' = 'p';
+  @Input('appTypography') variant: TypogoraphyFont = 'body-md';
   @Input() color: string = 'text-black';
-  @Input() fontWeight: string = 'font-normal';
+  @Input() fontWeight!: string | number;
   @Input() align: 'left' | 'center' | 'right' = 'left';
   @Input() extraClass: string = '';
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
-  ngOnChanges(): void {
+  ngOnChanges(changes: SimpleChanges): void {
     this.applyClasses();
+    this.applyStyle();
+
+    if (changes['fontWeight'] && this.fontWeight) {
+      this.renderer.setStyle(
+        this.el.nativeElement,
+        'fontWeight',
+        this.fontWeight
+      );
+    }
   }
 
   private applyClasses(): void {
@@ -36,7 +38,7 @@ export class TypographyDirective implements OnChanges {
       this.color,
       this.fontWeight,
       `text-${this.align}`,
-      this.mapVariantToFontSize(this.variant),
+      // this.mapVariantToFontSize(this.variant),
       this.extraClass,
     ]
       .filter(Boolean)
@@ -44,6 +46,15 @@ export class TypographyDirective implements OnChanges {
       .trim();
 
     this.renderer.setAttribute(this.el.nativeElement, 'class', baseClasses);
+  }
+
+  private applyStyle() {
+    const font = TypographyMap[this.variant];
+    this.renderer.setStyle(
+      this.el.nativeElement,
+      'font',
+      `var(${font.cssVar})`
+    );
   }
 
   private mapVariantToFontSize(variant: string): string {
@@ -59,13 +70,78 @@ export class TypographyDirective implements OnChanges {
       case 'h5':
         return 'text-lg';
       case 'h6':
-        return 'text-base font-medium';
+        return 'text-base font-md';
       case 'p':
         return 'text-base';
-      case 'small':
+      case 'sm':
         return 'text-sm';
       default:
         return 'text-base';
     }
   }
 }
+
+export type TypogoraphyFont =
+  | 'body-lg'
+  | 'body-md'
+  | 'body-sm'
+  | 'display-lg'
+  | 'display-md'
+  | 'display-sm'
+  | 'headline-lg'
+  | 'headline-md'
+  | 'headline-sm'
+  | 'label-lg'
+  | 'label-md'
+  | 'label-sm'
+  | 'title-lg'
+  | 'title-md'
+  | 'title-sm';
+
+export type Md3Font =
+  | 'body-large'
+  | 'body-medium'
+  | 'body-small'
+  | 'display-large'
+  | 'display-medium'
+  | 'display-small'
+  | 'headline-large'
+  | 'headline-medium'
+  | 'headline-small'
+  | 'label-large'
+  | 'label-medium'
+  | 'label-small'
+  | 'title-large'
+  | 'title-medium'
+  | 'title-small';
+
+const TypographyMap: Record<
+  TypogoraphyFont,
+  {
+    md3: Md3Font;
+    cssVar: string;
+  }
+> = {
+  'body-lg': { md3: 'body-large', cssVar: '--mat-sys-body-large' },
+  'body-md': { md3: 'body-medium', cssVar: '--mat-sys-body-medium' },
+  'body-sm': { md3: 'body-small', cssVar: '--mat-sys-body-small' },
+
+  'display-lg': { md3: 'display-large', cssVar: '--mat-sys-display-large' },
+  'display-md': { md3: 'display-medium', cssVar: '--mat-sys-display-medium' },
+  'display-sm': { md3: 'display-small', cssVar: '--mat-sys-display-small' },
+
+  'headline-lg': { md3: 'headline-large', cssVar: '--mat-sys-headline-large' },
+  'headline-md': {
+    md3: 'headline-medium',
+    cssVar: '--mat-sys-headline-medium',
+  },
+  'headline-sm': { md3: 'headline-small', cssVar: '--mat-sys-headline-small' },
+
+  'label-lg': { md3: 'label-large', cssVar: '--mat-sys-label-large' },
+  'label-md': { md3: 'label-medium', cssVar: '--mat-sys-label-medium' },
+  'label-sm': { md3: 'label-small', cssVar: '--mat-sys-label-small' },
+
+  'title-lg': { md3: 'title-large', cssVar: '--mat-sys-title-large' },
+  'title-md': { md3: 'title-medium', cssVar: '--mat-sys-title-medium' },
+  'title-sm': { md3: 'title-small', cssVar: '--mat-sys-title-small' },
+};
